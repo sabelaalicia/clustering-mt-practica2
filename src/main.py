b6fdb2908e80d0d82a14031750253feaf3a10029
Sabela <sabelalicia1@gmail.com>
2026-01-01 17:08:18 +0100

    parte opcional main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from representacion import crear_textos, vectorizacion_TF, vectorizacion_TFIDF
 from utils import plot_palabras_mas_frecuentes, plot_matrices_confusion, plot_wordcloud
-from agrupamiento import run_agrupamiento
+from agrupamiento import run_agrupamiento, run_metodo
 from evaluacion import run_evaluacion, plot_pca_tsne, filtrar_goldstandard
 from utils import plot_scatter_pca
 from sklearn.preprocessing import LabelEncoder
@@ -24,10 +24,10 @@ def main():
     X_tfidf, vectorizer_tfidf = vectorizacion_TFIDF(textos)
     print(f"Matriz TF-IDF generada: {X_tfidf.shape}")
 
-    #plot_palabras_mas_frecuentes(X_tf, vectorizer_tf, vector_type="TF", top_n=10, guardar=False)
-    #plot_palabras_mas_frecuentes(X_tfidf, vectorizer_tfidf, vector_type="TFIDF", top_n=10, guardar=False)
-    #plot_wordcloud(X_tf, vectorizer_tf, vector_type="TF", guardar=True)
-    #plot_wordcloud(X_tfidf, vectorizer_tfidf, vector_type="TFIDF", guardar=True)
+    plot_palabras_mas_frecuentes(X_tf, vectorizer_tf, vector_type="TF", top_n=10, guardar=False)
+    plot_palabras_mas_frecuentes(X_tfidf, vectorizer_tfidf, vector_type="TFIDF", top_n=10, guardar=False)
+    plot_wordcloud(X_tf, vectorizer_tf, vector_type="TF", guardar=True)
+    plot_wordcloud(X_tfidf, vectorizer_tfidf, vector_type="TFIDF", guardar=True)
 
     ####   Agrupamiento   ####
     results = run_agrupamiento(X_tf, X_tfidf, nombres_archivos)
@@ -41,9 +41,8 @@ def main():
 
     titles = ['TF - KMeans','TF - Agglomerative','TFIDF - KMeans','TFIDF - Agglomerative']
 
-    #plot_scatter_pca(matrices, labels_list, nombres=nombres_archivos, titles=titles, guardar=False)
-    
-    """
+    plot_scatter_pca(matrices, labels_list, nombres=nombres_archivos, titles=titles, guardar=False)
+
     ####   Evaluaciones   ####
     evals = run_evaluacion(results, ruta_goldstandard="data/goldstandard.csv",archivos_procesados=nombres_archivos)
     plot_matrices_confusion(evals, guardar=True)
@@ -94,7 +93,7 @@ def main():
         titulo="TF-IDF + Agglomerative",
         guardar_ruta="docs/images/pca_tsne_tfidf_agglomerative.pdf"
     )
-    """
+    
     
     ####   Opcional: Método Elbow   ####
     print("Ejecutando método Elbow para TF...")
@@ -102,6 +101,29 @@ def main():
 
     print("Ejecutando método Elbow para TF-IDF...")
     run_elbow_kmeans(X_tfidf, max_k=12, titulo="Elbow TF-IDF", guardar=True, ruta_guardado="docs/images/elbow_tfidf.png")
+
+    labels = run_metodo(X_tf, metodo="kmeans", n_clusters=9)
+    print(labels)
+
+    ####   Evaluación para k=9   ####
+    resultados_k9 = {
+        "tf_kmeans_k9": {
+            "labels": labels,
+            "nombres": nombres_archivos
+        }
+    }
+
+    eval_k9 = run_evaluacion(resultados_k9,ruta_goldstandard="data/goldstandard.csv",archivos_procesados=nombres_archivos)
+
+    metrica_k9 = eval_k9["tf_kmeans_k9"]
+
+    print("\n=== Métricas k=9 (TF + KMeans) ===")
+    print(f"Precisión (macro): {metrica_k9['precision']:.4f}")
+    print(f"Recall (macro): {metrica_k9['recall']:.4f}")
+    print(f"F1 (macro): {metrica_k9['f1']:.4f}")
+    print("Matriz de confusión:\n", metrica_k9["matriz_confusion"])
+
+    
 
 if __name__ == "__main__":
     main()
